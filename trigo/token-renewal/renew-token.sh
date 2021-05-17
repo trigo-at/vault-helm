7558eb9656337c4ac55cf9b4f021f24a5b6016f9
Daniel Freithofer <daniel.freithofer@trigo.at>
2021-05-17 18:43:06 +0200

    autorenewal working, error is sent to slack :)

--- trigo/token-renewal/renew-token.sh
+++ trigo/token-renewal/renew-token.sh
@@ -1,14 +1,59 @@
 #!/bin/bash
 
 set -x
-set -e
 
 OC_COMMAND="/app/oc-bin"
- #gets old token from secret in cluster
+SLACK_WEBHOOK_URL=$(${OC_COMMAND} get secret vault-backup-cron-job -n vault -o go-template  --template="{{.data.SLACK_WEBHOOK_URL}}" | base64 -d - )
+RENEW_URL="https://vault-backup.ocp.trigo.cloud:8200/v1/auth/token/renew-self"
+
+
+trap "exit 1" TERM
+export TOP_PID=$$
+
+_die() {
+    kill -s TERM $TOP_PID
+}
+
+send_slack() {
+	echo $1
+	status="good"
+	message="SUCCESS! ... "
+	if [[ $1 -eq 1 ]]; then
+		status="danger"; 
+		message="🐞🐞🐞 ";
+	fi
+
+	message="${message}$2"
+	
+msg=$(cat <<JSON
+{ 
+"color":"#ff0000",
+"mrkdwn": true,
+"attachments": [{
+	"fallback": "ERROR",
+	"color": "${status}",
+	"text": "${message}",
+	"mrkdwn_in": ["fields"]
+}]}
+JSON
+)
+	echo $msg
+	curl -X POST \
+		--data-urlencode "payload=${msg}" \
+		${SLACK_WEBHOOK_URL};
+}
 TOKEN=$(${OC_COMMAND} get secret  vault-unsealer -n vault -o go-template  --template="{{.data.VAULT_TOKEN}}" | base64 -d - )
 
- #creates new token from secondary vault in VM (orphan token is used as it will not expire when parent token expires)
-RESPONSE=$(curl --header "X-Vault-Token: ${TOKEN}" --request POST -d '"policies": ["autounseal", "autorenew"]' https://vault-backup.ocp.trigo.cloud:8200/v1/auth/token/create-orphan)
-NEWTOKEN=$( echo $RESPONSE | jq .auth.client_token)
+curl --fail --request POST --header "X-Vault-Token: ${TOKEN}" ${RENEW_URL}; echo $? > /tmp/error;
+ERRORFILE=$(cat /tmp/error)
 
-${OC_COMMAND} process -n vault -p NEWTOKEN=${NEWTOKEN} -f /app/vault-unsealer.yaml | ${OC_COMMAND}  apply -n vault -f -
+checkResponse() {
+	if [ "${ERRORFILE}" != "0" ]; then
+		send_slack 1 "*ERROR: renewing vault unseal token with Curl ErrorCode: ${ERRORFILE}"
+		_die
+	else
+		send_slack 0 "Succesfully renewed periodic lease for auto unseal token"
+	fi
+}
+
+checkResponse
